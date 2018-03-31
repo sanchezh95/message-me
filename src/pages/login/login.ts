f9160cd5fcaa5faf57c51281ff93175cf58d056f
Huimee Sanchez <sanchezh95@gmail.com>
2018-03-30 17:22:59 -0700

    sign up and login added

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { GroupPage } from '../group/group';
 
+import * as firebase from 'firebase';
 @IonicPage()
 @Component({
   selector: 'page-login',
@@ -10,9 +11,53 @@ import { GroupPage } from '../group/group';
 })
 export class LoginPage {
 
-  data = { screenName: "" };
+  data = {
+    email: "",
+    password: "",
+    screenName: ""
+  };
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  // Creates new user
+  signup() {
+    firebase.auth().createUserWithEmailAndPassword(this.data.email, this.data.password)
+      .then(success => {
+        console.log('Signup successful: ', success);
+        this.enterScreenName();
+      })
+
+      .catch(function(err) {
+        var errCode = err.code;
+        var errMsg = err.message;
+        if (errCode === 'auth/weak-password') {
+          alert('The password is too weak');
+        } else {
+          alert(errMsg);
+        }
+        console.log(err)
+      });
+    this.enterScreenName();
+  }
+
+  // Login existing user
+  login() {
+    firebase.auth().signInWithEmailAndPassword(this.data.email, this.data.password)
+      .then(success => {
+        console.log('Login successful: ', success);
+        this.enterScreenName();
+      })
+      .catch(function(err) {
+        var errCode = err.code;
+        var errMsg = err.message;
+        if (errCode === 'auth/wrong-password') {
+          alert('Wrong password');
+        } else {
+          alert(errMsg);
+        }
+        console.log(err);
+      });
   }
 
   enterScreenName() {
